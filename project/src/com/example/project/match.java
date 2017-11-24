6f84486a0bc97ffc9f0b377136da33016eedff68
yinzhibo <1350526138@qq.com>
2017-11-24 16:32:39 +0800

    --"美化比赛通知界面”

--- project/src/com/example/project/match.java
+++ project/src/com/example/project/match.java
@@ -35,9 +35,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class match extends Activity{
-	String user="";
-    String name="";
-    private ArrayList<String> matchID = new ArrayList<String>();
+	private String user="";
+    private String name="";
+    boolean is_signed = false;
+    private ArrayList<String> matchID_ing = new ArrayList<String>();
+    private ArrayList<String> matchID_ed = new ArrayList<String>();
+    private ArrayList<String> matchname_ing = new ArrayList<String>();
+    private ArrayList<String> matchname_ed = new ArrayList<String>();
 	Handler handler1;
     @Override 
     protected void onCreate(Bundle savedInstanceState){
@@ -48,17 +52,14 @@ public class match extends Activity{
         v.setBackgroundColor(Color.WHITE);
         final View vv = (View)findViewById(R.id.left);
         vv.setBackgroundColor(Color.RED);
-//     	RelativeLayout r = (RelativeLayout)findViewById(R.id.rr);
-//		View v = new View(this);
-//		v.setBackgroundColor(Color.RED);
-//		android.widget.RelativeLayout.LayoutParams param0 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-//		param0.height = 3;
-//        param0.width = 300;
-//		param0.leftMargin = 10;
-//		param0.topMargin = 40;
-//		r.addView(v,param0);
+        final Button listen_back = (Button)findViewById(R.id.back);
+        listen_back.setOnClickListener(new listen_back());
+        Intent intent = getIntent();
+		Bundle data = intent.getExtras();
+		name = data.getString("name");
+		user = data.getString("user");
         init();
-     	draw_match();
+     	draw_match(is_signed);
 		final Button signed = (Button)findViewById(R.id.signed);
 		signed.setOnClickListener(new OnClickListener() {
 			
@@ -69,6 +70,10 @@ public class match extends Activity{
 				left.setBackgroundColor(Color.WHITE);
 				final View right = (View)findViewById(R.id.right);
 				right.setBackgroundColor(Color.RED);
+				if(!is_signed){
+					is_signed = true;
+					draw_match(is_signed);
+				}
 			}
 		});
 		final Button signing = (Button)findViewById(R.id.signing);
@@ -81,54 +86,66 @@ public class match extends Activity{
 				left.setBackgroundColor(Color.RED);
 				final View right = (View)findViewById(R.id.right);
 				right.setBackgroundColor(Color.WHITE);
+				if(is_signed){
+					is_signed = false;
+					draw_match(is_signed);
+				}
 			}
 		});
     }
     public void init(){
         for(int i = 1 ; i < 15 ; i++){
-        	matchID.add(""+i+"");
+        	matchID_ing.add(""+i+"");
+        	matchID_ed.add(""+i+"");
+        	matchname_ing.add("这里是赛事"+i+"");
+        	matchname_ed.add("这里是结束的赛事"+i+"");
         }
+        
     }
-    public void draw_match(){
-//     	RelativeLayout r = (RelativeLayout)findViewById(R.id.rr);
-//		View v = new View(this);
-//		v.setBackgroundResource(R.drawable.bai1);
-//		android.widget.RelativeLayout.LayoutParams param0 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-//		param0.height = 1000;
-//        param0.width = 300;
-//		param0.leftMargin = 10;
-//		param0.topMargin = 40;
-//		r.addView(v,param0);
-//		android.widget.RelativeLayout.LayoutParams param1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-//		View vv = new View(this);
-//		vv.setBackgroundResource(R.drawable.bai1);
-//		param1.topMargin=1200;
-//		param1.height = 1000;
-//        param1.width = 300;
-//		param1.leftMargin = 10;
-//		r.addView(vv,param1);
+    public void draw_match(boolean sign){
+    	int listlength = 0;
     	RelativeLayout r = (RelativeLayout)findViewById(R.id.rr);
-    	for(int i = 0 ; i < matchID.size(); i++ ){
+    	r.removeAllViews();
+    	if(!is_signed){
+    		listlength = matchID_ing.size();
+    	}else{
+    		listlength = matchID_ed.size();
+    	}
+    	for(int i = 0 ; i < listlength; i++ ){
     		View vv = new View(this);
 			android.widget.RelativeLayout.LayoutParams param0 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
 			param0.height = 300;
 		    param0.width = 1200;
 			param0.leftMargin = 1;
 			param0.topMargin = 1 + 310*i;
-			//param0.setMargins(0, 10, 100, 100);
 			vv.setBackgroundResource(R.drawable.blue);
 			r.addView(vv,param0);
+			final TextView text = new TextView(this);
+			android.widget.RelativeLayout.LayoutParams param1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+			if(!is_signed){
+				text.setText(matchname_ing.get(i));
+			}
+			else{
+				text.setText(matchname_ed.get(i));
+			}
+			text.setTextColor(Color.RED);
+			text.setTextSize(13);
+			param1.height = 60;
+			param1.width = 500;
+			param1.topMargin = 80 + 310*i;
+			param1.leftMargin = 120;
+			r.addView(text,param1);
 		    final Button bu = new Button(this);
 			android.widget.RelativeLayout.LayoutParams param4 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-			param4.setMargins(950,30+i*130,70,1100-i*130);
+//			param4.setMargins(950,30+i*130,70,1100-i*130);
 			bu.setText("进入赛事");
 			bu.setTextColor(Color.RED);
 			bu.setBackgroundResource(R.drawable.login_shape);
 			bu.setTextSize(11);
 			param4.height = 90;
 			param4.width = 230;
-			param4.leftMargin = 380;
-			param4.topMargin = 80 + 310*i;		
+			param4.leftMargin = 730;
+			param4.topMargin = 210 + 310*i;		
 			r.addView(bu,param4);
 			bu.setOnClickListener(new OnClickListener() {
 				
@@ -140,6 +157,18 @@ public class match extends Activity{
 			});
     	}
     }
+	class listen_back implements OnClickListener{
+		@Override
+    	public void onClick(View v){
+    		Bundle data = new Bundle();
+    		Intent intent = new Intent(match.this,MainActivity.class);//test其实是login
+			data.putString("user",user);
+			data.putString("name", name);
+			intent.putExtras(data);
+			startActivity(intent);
+			finish();
+    	}
+	}
 
 
 }
